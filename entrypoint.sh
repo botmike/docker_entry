262b9588e9b38633abe0c5675c9d18a0b2e20692
botmike <ai.mikepage@gmail.com>
2020-09-02 21:33:43 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # x11vnc_display.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server
 export DISPLAY=:20
-Xvfb :20 -screen 0 800x600x24 &
+Xvfb :20 -screen 0 1920x1200x24 &
 x11vnc -passwd TestVNC -display :20 -N -forever 
 # after this, we could start running the tests
