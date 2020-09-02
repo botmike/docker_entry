d426b2de86c2da614c0a01165c2cad210007f16a
botmike <ai.mikepage@gmail.com>
2020-09-02 16:53:33 +0100

    Create x11vnc_display.sh

---
+++ x11vnc_display.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# x11vnc_display.sh file for starting the xvfb with better screen resolution, configuring and running the vnc server
+export DISPLAY=:20
+Xvfb :20 -screen 0 1366x768x16 &
+x11vnc -passwd TestVNC -display :20 -N -forever 
